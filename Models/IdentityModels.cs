a2d37365bdc84b9d21599695c8b1fc51fefd4f1f
Lethanhquang165 <lethanhquang165@gmail.com>
2017-01-09 17:06:09 +0700

    Fix code

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -6,7 +6,8 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace WebApplication1.Models
 {
 	
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.e
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
